f87718027d766113a7607b3344d500801b4d78c9
Jexxy1517 <ferdicinere13@gmail.com>
2025-11-03 06:06:00 +0700

    commit part 3

--- app/src/main/java/com/example/lab_week_09/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_09/MainActivity.kt
@@ -9,10 +9,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -25,9 +23,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.lab_week_09.ui.theme.LAB_WEEK_09Theme
+import com.example.lab_week_09.ui.theme.OnBackgroundItemText
+import com.example.lab_week_09.ui.theme.OnBackgroundTitleText
+import com.example.lab_week_09.ui.theme.PrimaryTextButton
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -77,10 +77,12 @@ fun HomeContent(
     LazyColumn {
         item {
             Column(
-                modifier = Modifier.padding(16.dp).fillMaxSize(),
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(
+                OnBackgroundTitleText(
                     text = stringResource(
                         id = R.string.enter_item
                     )
@@ -94,23 +96,23 @@ fun HomeContent(
                         onInputValueChange(it)
                     }
                 )
-                Button(onClick = {
-                    onButtonClick()
-                }) {
-                    Text(
-                        text = stringResource(
-                            id = R.string.button_click
-                        )
+                PrimaryTextButton(
+                    text = stringResource(
+                        id = R.string.button_click
                     )
+                ) {
+                    onButtonClick()
                 }
             }
         }
         items(listData) { item ->
             Column(
-                modifier = Modifier.padding(vertical = 4.dp).fillMaxSize(),
+                modifier = Modifier
+                    .padding(vertical = 4.dp)
+                    .fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(text = item.name)
+                OnBackgroundItemText(text = item.name)
             }
         }
     }
--- app/src/main/java/com/example/lab_week_09/ui/theme/Color.kt
+++ app/src/main/java/com/example/lab_week_09/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.example.lab_week_09.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val CustomLightBlue = Color(0xFF81D4FA)
+val CustomGray = Color(0xFF9E9E9E)
+val CustomDarkGray = Color(0xFF616161)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val CustomDarkBlue = Color(0xFF03A9F4)
+val CustomDarkerGray = Color(0xFF424242)
+val CustomLightGray = Color(0xFFBDBDBD)
